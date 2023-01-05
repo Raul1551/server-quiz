98f804f50538199039e0e2a2683f4f21a774f747
raul <raulfernandez484@gmail.com>
2023-01-05 15:26:33 +0100

    nueva consulta modificada

--- index.js
+++ index.js
@@ -32,7 +32,11 @@ app.post('/create', (req, res) => {
     let resultado = req.body;
 
     // const insert = `INSERT INTO users (id, nombre, apellido, email, phone, perfil) VALUES (NULL, '${JSON.stringify(bdatos)}', "rodriguez", "rodrig@gmail.com", "756898766", "gastronomico")`;
-    const insert = `INSERT INTO users (id, nombre, apellido, email, telefono, perfil, respuestas) VALUES (NULL, '${resultado[0]}', '${resultado[1]}', '${resultado[2]}', '${resultado[3]}', '${resultado[4]}', '${resultado[5]}')`;
+    //const insert = `INSERT INTO users (id, nombre, apellido, email, telefono, perfil, respuestas) VALUES (NULL, '${resultado[0]}', '${resultado[1]}', '${resultado[2]}', '${resultado[3]}', '${resultado[4]}', '${resultado[5]}')`;
+    const insert = `INSERT INTO users (id, nombre, apellido, email, telefono, perfil, pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, pregunta6, pregunta7, pregunta8, pregunta9,
+        pregunta10, pregunta11, pregunta12, pregunta13, pregunta14) VALUES (NULL, '${resultado[0]}', '${resultado[1]}', '${resultado[2]}',
+        '${resultado[3]}', '${resultado[4]}', '${resultado[5]}', '${resultado[6]}', '${resultado[7]}', '${resultado[8]}', '${resultado[9]}', '${resultado[10]}',
+        '${resultado[11]}', '${resultado[12]}', '${resultado[13]}', '${resultado[14]}', '${resultado[15]}', '${resultado[16]}', '${resultado[17]}', '${resultado[18]}')`;
     connector.query(insert, (err, result) => {
         if (err) throw err;
         res.send(JSON.stringify('Datos guardados'));
